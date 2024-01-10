7722d9c17432df4bc25db5a9f38685954352608d
Jesper Geerling <s1113880@student.hsleiden.nl>
2024-01-10 14:59:35 +0100

    pls werk ik raak uit ideeen

--- src/main/java/com/example/Iprwcbackend/IprwcBackendApplication.java
+++ src/main/java/com/example/Iprwcbackend/IprwcBackendApplication.java
@@ -2,12 +2,14 @@ package com.example.Iprwcbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @SpringBootApplication
 @RestController
+@EnableJpaRepositories(basePackages = {"com.example.Iprwcbackend.module.user", "com.example.Iprwcbackend.module.product", "com.example.Iprwcbackend.module.category", "com.example.Iprwcbackend.module.order"})
 public class IprwcBackendApplication {
 
 	public static void main(String[] args) {

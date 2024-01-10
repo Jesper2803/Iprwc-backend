7bd57ef674ab01de9a320aa3aabef1d7b9209eab
Jesper Geerling <s1113880@student.hsleiden.nl>
2024-01-10 15:24:40 +0100

    plsplspslpslsplpsls2

--- src/main/java/com/example/Iprwcbackend/IprwcBackendApplication.java
+++ src/main/java/com/example/Iprwcbackend/IprwcBackendApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableJpaRepositories(basePackages = {"com.example.Iprwcbackend.module.user", "com.example.Iprwcbackend.module.product", "com.example.Iprwcbackend.module.category", "com.example.Iprwcbackend.module.order"})
+//@EnableJpaRepositories(basePackages = {"com.example.Iprwcbackend.module.user", "com.example.Iprwcbackend.module.product", "com.example.Iprwcbackend.module.category", "com.example.Iprwcbackend.module.order"})
 public class IprwcBackendApplication {
 
 	public static void main(String[] args) {
